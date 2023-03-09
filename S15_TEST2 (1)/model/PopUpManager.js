65215af00d16340a944886e86e93f68c0e21ee81
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-09 14:10:47 +0200

    Create PopUpManager.js

---
+++ S15_TEST2 (1)/model/PopUpManager.js
@@ -0,0 +1,18 @@
+let popup = document.getElementById("loginPopUp");
+let registrationPopUp = document.getElementById('registrationPopUp');
+
+function openPopUp(){
+    popup.style.display = "inline-block";
+}
+
+function closePopUp(){
+    popup.style.display = "none";
+}
+
+function openRegPopUp(){
+    registrationPopUp.style.display = "inline-block"
+}
+
+function closeRegPopUp(){
+    registrationPopUp.style.display = "none";
+}
