f5f6910c7883fdacfe01c5f14d1449c1e90c9348
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-10 21:20:46 +0200

    Update UserManager.js

--- S15_TEST2 (1)/model/UserManager.js
+++ S15_TEST2 (1)/model/UserManager.js
@@ -1,50 +1,45 @@
 class User {
-    constructor (name, pass){
-        this.name = name,
-        this.pass = pass
-    }
+  constructor(name, pass) {
+    (this.name = name), (this.pass = pass);
+  }
 }
 
 class UserManager {
-        constructor() {
-            let logedUser = JSON.parse(localStorage.getItem("loged")) || null;
-            if(logedUser){
-                this.logedUser = new User(logedUser.name, logedUser.pass);
-            }
-        }
+  constructor() {
+    let logedUser = JSON.parse(localStorage.getItem("loged")) || null;
+    if (logedUser) {
+      this.logedUser = new User(logedUser.name, logedUser.pass);
+    }
+  }
 
+  usersList = JSON.parse(localStorage.getItem("usersList")) || [];
+  logedUser = null;
 
+  logIn = (name, pass) => {
+    let existingUser = this.usersList.filter(
+      (e) => e.name === name && e.pass === pass
+    );
 
-    usersList = JSON.parse(localStorage.getItem("usersList")) || [];
-    logedUser = null;
-
-    logIn = (name, pass) => {
-        
-        let existingUser = this.usersList.filter(e => 
-            e.name === name && e.pass === pass
-        );
-
-        if(existingUser.length > 0){
-            this.logedUser = existingUser;
-            localStorage.setItem('loged', JSON.stringify(this.logedUser));
-            return true;
-        }
-
-        return false;
+    if (existingUser.length > 0) {
+      this.logedUser = existingUser;
+      localStorage.setItem("loged", JSON.stringify(this.logedUser));
+      return true;
     }
 
-    logOut = () => {
-        localStorage.removeItem("loged");
-    }
+    return false;
+  };
 
-    createUser = (name, pass, confirm) => {
-        let existingUser = this.usersList.filter(e => e.name === name);
-        
+  logOut = () => {
+    localStorage.removeItem("loged");
+  };
 
-        if(existingUser.length === 0){
-            this.usersList.push(new User(name, pass));
-            localStorage.setItem('usersList', JSON.stringify(this.usersList));
-        }
+  createUser = (name, pass, confirm) => {
+    let existingUser = this.usersList.filter((e) => e.name === name);
+   
 
-    }
+    if (existingUser.length === 0) {
+      this.usersList.push(new User(name, pass));
+      localStorage.setItem("usersList", JSON.stringify(this.usersList));
+    } 
+  };
 }
